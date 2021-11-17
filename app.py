48e609118168584c29366c0785c78b0e4f44b90b
sebaspv <poncegramming@gmail.com>
2021-11-16 23:27:42 -0600

    fix

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 import uvicorn
+from Questgen import main
 
 app = FastAPI()
 
-@app.get("/")
-async def hello():
-    return {"message":"it works!"}
+model = main.QGen()
+@app.get("/api/faq/{prompt}")
+async def hello(prompt: str):
+    payload = {"input_text": prompt}
+    output = model.predict_shortq(payload)
+    return output
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
